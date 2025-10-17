cf97ab2a9adfd828e5f270a67d48baa22d9f5002
tharunsiddharths <tharun.siddharth@gmail.com>
2025-10-17 12:51:27 +0100

    Creating a Release

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM openjdk:latest
-COPY ./target/semethods-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
+
+# Copy the updated JAR file with version 0.1.0.2 or 0.1-alpha-2
+COPY ./target/seMethods-0.1.0.2-jar-with-dependencies.jar /tmp
+
 WORKDIR /tmp
-ENTRYPOINT ["java", "-jar", "semethods-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+
+# Update entry point with the new JAR file name
+ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.2-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,26 +1,28 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        // Connect to MongoDB on local system - we're using port 27000
-        // Connect to MongoDB
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
-        // Get a database - will create when we use it
+public class App {
+    public static void main(String[] args) {
+        // Connect to MongoDB - using MongoClients.create() for MongoDB 4.x+
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017"); // Adjust your MongoDB connection string as needed
+
+        // Get a database - will create it when used
         MongoDatabase database = mongoClient.getDatabase("mydb");
+
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
+
         // Create a document to store
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "DevOps")
                 .append("year", "2024")
                 .append("result", new Document("CW", 95).append("EX", 85));
+
         // Add document to collection
         collection.insertOne(doc);
 
